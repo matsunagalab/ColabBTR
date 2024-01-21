e9166de55209606f8f69cc5c8e4c5d8eb1b2108c
ymatsunaga <ymatsunaga@mail.saitama-u.ac.jp>
2024-01-21 11:56:52 +0900

    add molecule surface height calculation (surfing)

--- colabbtr/morphology.py
+++ colabbtr/morphology.py
@@ -97,3 +97,105 @@ def differentiable_btr(images, tip_size, nepoch=100, lr=0.1, weight_decay=0.0, d
 
     tip_estimate = tip.detach()
     return tip_estimate, loss_train
+
+def surfing(xyz, radius, config):
+    """
+    compute the maximum height (z-value) of molecular surface at grid points on AFM stage (where z=0)
+    """
+    radius2 = radius**2
+    x_stage = torch.arange(config["min_x"], config["max_x"], config["resolution_x"]) + 0.5*config["resolution_x"]
+    y_stage = torch.arange(config["min_y"], config["max_y"], config["resolution_y"]) + 0.5*config["resolution_y"]
+    z_stage = torch.full((len(y_stage), len(x_stage)), xyz[:, 2].min())
+    for i in range(len(x_stage)):
+        for j in range(len(y_stage)):
+            x = x_stage[i]
+            y = y_stage[j]
+            dx = xyz[:, 0] - x
+            dy = xyz[:, 1] - y
+            r2 = dx**2 + dy**2
+            index_within_radius = r2 < radius2
+            #print(r2[:3], radius[:3])
+            if any(index_within_radius):
+                z_stage[-j-1, i] = torch.max(xyz[index_within_radius, 2] + torch.sqrt(radius2[index_within_radius] - r2[index_within_radius]))
+    return z_stage
+
+# mapping atom name to radius in Angstrom
+Atom2Radius = {
+    "H": 1.20,
+    "HE": 1.40,
+    "B": 1.92,
+    "C": 1.70,
+    "CA": 1.70,
+    "CB": 1.70,
+    "CG": 1.70,
+    "CG1": 1.70,
+    "CG2": 1.70,
+    "CG3": 1.70,
+    "CD": 1.70,
+    "CD1": 1.70,
+    "CD2": 1.70,
+    "CD3": 1.70,
+    "CZ": 1.70,
+    "CZ1": 1.70,
+    "CZ2": 1.70,
+    "CZ3": 1.70,
+    "CE": 1.70,
+    "CE1": 1.70,
+    "CE2": 1.70,
+    "CE3": 1.70,
+    "CH": 1.70,
+    "CH1": 1.70,
+    "CH2": 1.70,
+    "CH3": 1.70,
+    "N": 1.55,
+    "NE": 1.55,
+    "NZ": 1.55,
+    "ND1": 1.55,
+    "ND2": 1.55,
+    "NE1": 1.55,
+    "NE2": 1.55,
+    "NH1": 1.55,
+    "NH2": 1.55,
+    "O": 1.52,
+    "OH": 1.52,
+    "OG": 1.52,
+    "OE1": 1.52,
+    "OE2": 1.52,
+    "OG1": 1.52,
+    "OG2": 1.52,
+    "OD1": 1.52,
+    "OD2": 1.52,
+    "OXT": 1.52,
+    "F": 1.47,
+    "MG": 1.73,
+    "AL": 1.84,
+    "SI": 2.10,
+    "P": 1.80,
+    "S": 1.80,
+    "SD": 1.80,
+    "SG": 1.80,
+    "CL": 1.75,
+    "AR": 1.88,
+    "K": 2.75,
+    "CYS": 2.75,
+    "PHE": 3.2,
+    "LEU": 3.1,
+    "TRP": 3.4,
+    "VAL": 2.95,
+    "ILE": 3.1,
+    "MET": 3.1,
+    "HIS": 3.05,
+    "HSD": 3.05,
+    "TYR": 3.25,
+    "ALA": 2.5,
+    "GLY": 2.25,
+    "PRO": 2.8,
+    "ASN": 2.85,
+    "THR": 2.8,
+    "SER": 2.6,
+    "ARG": 3.3,
+    "GLN": 3.0,
+    "ASP": 2.8,
+    "LYS": 3.2,
+    "GLU": 2.95
+}
